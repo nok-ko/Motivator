443639d2e6cd61738d90790bb18f8cb6689bf377
Maxwell Babey <babeymax@gmail.com>
2021-11-03 13:17:55 -0700

    Change API codes. Add firebase_API.js to .gitignore

--- scripts/firebase_API.js
+++ scripts/firebase_API.js
@@ -1,12 +1,13 @@
  // Your web app's Firebase configuration
     // For Firebase JS SDK v7.20.0 and later, measurementId is optional
     const firebaseConfig = {
-        apiKey: "AIzaSyDFtwFONXnYzs_vUkKrQKpI4dWe0bVzPRU",
-        authDomain: "motivator-75efd.firebaseapp.com",
-        projectId: "motivator-75efd",
-        storageBucket: "motivator-75efd.appspot.com",
-        messagingSenderId: "645651840465",
-        appId: "1:645651840465:web:17886176c1c662417c34f0",
+      apiKey: "AIzaSyDWJw300W2mUfM6zTkm19GHF0X8OWVTm6Q",
+      authDomain: "motivator-8ef19.firebaseapp.com",
+      projectId: "motivator-8ef19",
+      storageBucket: "motivator-8ef19.appspot.com",
+      messagingSenderId: "600712119095",
+      appId: "1:600712119095:web:c7ced125a34e216f4710b2",
+      measurementId: "G-98BYLBDY50"
       };
   
       // Initialize Firebase
