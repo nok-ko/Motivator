9c6204575a67d1a90150dfed413baa1ee075ef1f
Nokko <13428215+nok-ko@users.noreply.github.com>
2021-11-05 15:44:37 -0700

    Clean up stray `console.log`s etc.

--- scripts/profile.js
+++ scripts/profile.js
@@ -5,31 +5,22 @@
 
 const db = firebase.firestore(app);
 
-// class Goal {
-// 	static fromDocument(doc) {
-// 		const goal = new Goal();
-// 		goal.description = doc.
-// 	}
-// }
-
 // TODO: documentation pass
 function listGoals() {
 	const userID = firebase.auth().currentUser.uid; // assume current user is logged in
 	const goalsContainer = document.getElementById("user_goals")
 
-	console.log("listing goals")
+	// TODO: Show/hide “loading” message while we pull in data
+	// TODO: live-update goals
+
 	db.collection('users').doc(userID).collection('goals').get().then(
 		(goals) => {
-			console.log("goals?")
-			console.log(goals)
 			goals.forEach((goal) => {
-				console.log("goal?")
-				console.log(goal.data())
 				const goalEl = document.createElement("div")
 				const amountGoal = goal.data().amountGoal;
 				const amount = goal.data().amount;
 				goalEl.classList.add("goal");
-				
+
 				// FIXME I think this is questionable style, but… it's fast to implement
 				goalEl.innerHTML = `
 					<p class="goal-description">${goal.data().description}</p>
@@ -48,25 +39,22 @@ function listGoals() {
 							<!-- insert icon here -->
 							Due by ${new Date(goal.data().dateEnd.seconds*1000).toDateString()}
 						</span>
-					</div>`
-				goalEl.querySelector(".progressbar-fill").addEventListener("animationend", onEnd, false);
-				goalsContainer.appendChild(goalEl); //TODO: use a fragment here
+					</div>`;
+				
+				// Clean up animations once they finish
+				goalEl.querySelector(".progressbar-fill").addEventListener("animationend", 
+					function () {
+						this.classList.add("progress-anim-finished")
+					}, false);
+				// TODO: use a DocumentFragment when appending?
+				goalsContainer.appendChild(goalEl);
 			})
-			// console.log(next);
 		}
 	);
-
-	// Clean up animations once they finish
-	function onEnd() {
-		this.classList.add("progress-anim-finished")
-		console.log(this)
-	}
 }
 
 // First, log in…
 firebase.auth().onAuthStateChanged(function (user) {
-		console.log("auth state changed ")
-		console.log(firebase.auth().currentUser.uid)
         if (user) { 
 			// Now we have a reference to the logged in user!
 			// Update the page, list the goals of the currently signed-in user.
