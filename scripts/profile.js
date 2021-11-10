48e5a1d43020392a641edd5178d9f4125e04ea7f
Maxwell Babey <babeymax@gmail.com>
2021-11-10 12:21:02 -0800

    Enable editing and DB integration for Bio

--- scripts/profile.js
+++ scripts/profile.js
@@ -25,7 +25,7 @@ function listGoals() {
 				goalEl.innerHTML = `
 					<p class="goal-description">${goal.data().description}</p>
 					<svg class="progressbar" version="1.1" viewBox="0 0 100 200" preserveAspectRatio="none"
-					style="--progressbar-finish-percent:${Math.floor(amount/amountGoal * 100)}%"
+					style="--progressbar-finish-percent:${Math.floor(amount / amountGoal * 100)}%"
 					>
 						<rect fill="#CCCCCC" stroke-width="10" x="0" y="0" width="100" height="200" />
 						<rect class="progressbar-fill" fill="#888888" stroke-width="10" x="0" y="0" width="100"
@@ -37,12 +37,12 @@ function listGoals() {
 						</span>
 						<span class="goal-deadline">
 							<!-- insert icon here -->
-							Due by ${new Date(goal.data().dateEnd.seconds*1000).toDateString()}
+							Due by ${new Date(goal.data().dateEnd.seconds * 1000).toDateString()}
 						</span>
 					</div>`;
-				
+
 				// Clean up animations once they finish
-				goalEl.querySelector(".progressbar-fill").addEventListener("animationend", 
+				goalEl.querySelector(".progressbar-fill").addEventListener("animationend",
 					function () {
 						this.classList.add("progress-anim-finished")
 					}, false);
@@ -53,11 +53,76 @@ function listGoals() {
 	);
 }
 
-// First, log in…
-firebase.auth().onAuthStateChanged(function (user) {
-        if (user) { 
+var currentUser;
+function populateInfo() {
+	firebase.auth().onAuthStateChanged(user => {
+		// Check if user is signed in:
+		if (user) {
 			// Now we have a reference to the logged in user!
+			currentUser = db.collection("users").doc(user.uid);
 			// Update the page, list the goals of the currently signed-in user.
 			listGoals();
+
+			//go to the correct user document by referencing to the user uid
+			//get the document for current user.
+			currentUser.onSnapshot(userDoc => {
+				//get the data fields of the user
+				var userName = userDoc.data().name;
+				var userEmail = userDoc.data().email;
+				var userBio = userDoc.data().bio;
+
+				//if the data fields are not empty, then write them in to the form.
+				// if (userName != null) {
+				// 	document.getElementById("userName").value = userName;
+				// }
+				// if (userEmail != null) {
+				// 	document.getElementById("userEmail").value = userEmail
+				// }
+				if (userBio != null) {
+					document.getElementById("bioInput").value = userBio;
+					document.getElementById("bioText").textContent = userBio;
+				}
+			})
+		} else {
+			// No user is signed in.
+			console.log("No user is signed in");
 		}
 	});
+}
+populateInfo();
+
+//Enable editing for bio field.
+function editBio() {
+	//Enable the form fields. Makes the paragraph text invisible and
+	//	turn the form on.
+	document.getElementById('bioField').disabled = false;
+	document.getElementById('bioInput').hidden = false;
+	document.getElementById('bioText').hidden = true;
+	//Disappear the edit button and show the save button.
+	document.getElementById('editBio').hidden = true;
+	document.getElementById('saveBio').hidden = false;
+}
+onclick = "editBio()"
+
+//Save current form input to Firebase. Disable editing of bio field.
+function saveBio() {
+	userBio = document.getElementById("bioInput").value;
+
+	//Update the Firebase.
+	currentUser.update({
+		bio: userBio
+	})
+		.then(() => {
+			console.log("Document successfully updated!");
+		})
+
+	//Disable editing of the form fields. Makes the form invisible and
+	//	turns the paragraph text on.
+	document.getElementById("bioField").disabled = true;
+	document.getElementById('bioInput').hidden = true;
+	document.getElementById('bioText').hidden = false;
+	//Disappear the save button and show the edit button.
+	document.getElementById('editBio').hidden = false;
+	document.getElementById('saveBio').hidden = true;
+}
+onclick = "saveUserInfo()"
